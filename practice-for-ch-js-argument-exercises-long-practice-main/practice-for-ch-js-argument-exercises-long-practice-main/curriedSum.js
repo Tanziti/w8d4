cd0b7a4682b459290af49df001466227b1a1d5c7
Vincent <vshan0816@gmail.com>
2023-08-17 16:12:10 -0400

    finish curriedSum

--- practice-for-ch-js-argument-exercises-long-practice-main/practice-for-ch-js-argument-exercises-long-practice-main/curriedSum.js
+++ practice-for-ch-js-argument-exercises-long-practice-main/practice-for-ch-js-argument-exercises-long-practice-main/curriedSum.js
@@ -1,9 +1,16 @@
-function curriedSum(int) {
-  let count = 0;
-  
-    return function (arg) {
-      count += arg
-    }
+function curriedSum(numArgs) {
+  let numbers = []
 
-  
-}
+  return function _curriedSum(num) {
+    numbers.push(num);
+    if (numbers.length === numArgs) {
+      return numbers.reduce((acc, el) => acc + el);
+    } else {
+      return _curriedSum;
+    };
+  };
+};
+
+const sum = curriedSum(4);
+console.log(sum(5)(30)(20)(1)); // => 56
+ 
