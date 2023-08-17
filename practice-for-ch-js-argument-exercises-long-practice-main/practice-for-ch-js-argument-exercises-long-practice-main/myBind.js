1abec0dbd57b5d23ffa2f6db82f3bcbc8e0887a8
Vincent <vshan0816@gmail.com>
2023-08-17 15:11:26 -0400

    Finish myBind One

--- practice-for-ch-js-argument-exercises-long-practice-main/practice-for-ch-js-argument-exercises-long-practice-main/myBind.js
+++ practice-for-ch-js-argument-exercises-long-practice-main/practice-for-ch-js-argument-exercises-long-practice-main/myBind.js
@@ -16,22 +16,41 @@ class Dog {
 }
 
 
+// Function.prototype.myBind = function (ctx) {
+//   let that  = this;
+//   let result = []; 
+//   for (let i = 0; i < arguments.length; i++){
+//     result.push(arguments[i]);
+
+//   }
+//   return function() {
+//     return that.apply(ctx, result.slice(1))
+//   }
+
+
+// }
+
 Function.prototype.myBind = function (ctx) {
-  let that  = this;
-  let result = []; 
-  for (let i = 0; i < arguments.length; i++){
-    result.push(arguments[i]);
-
+    let that  = this;
+    let result = []; 
+    for (let i = 0; i < arguments.length; i++){
+      result.push(arguments[i]);
+  
+    }
+    return function(callArgs) {
+      let allArgs = result.concat(callArgs)
+      return that.apply(ctx, allArgs.slice(1))
+    }
+  
+  
   }
-  return function() {
-    return that.apply(ctx, result.slice(1))
-  }
-
-
-}
 
 const markov = new Cat("Markov");
 const pavlov = new Dog("Pavlov");
 
 console.log(markov.says.myBind(pavlov, "meow", "Kush")());
 console.log(markov.says.myBind(pavlov)("meow", "a tree"));
+console.log(markov.says.myBind(pavlov, "meow")("Markov"));
+
+const notMarkovSays = markov.says.myBind(pavlov);
+console.log(notMarkovSays("meow", "me"));
